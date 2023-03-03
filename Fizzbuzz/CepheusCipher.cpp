c62dab2d3e5eac7e7d27b4e2cf8303128dd7a436
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-03-03 18:48:28 +0530

    Update CepheusCipher.cpp

--- Fizzbuzz/CepheusCipher.cpp
+++ Fizzbuzz/CepheusCipher.cpp
@@ -1,3 +1,69 @@
+/*
+Recruitment of the Cepheus Cipher is underway!! Selection is done based on a game which is played by two competitors Cipher1 and Cipher2 . Winner will be promoted to the position of Cepheus Cipher. There are n planets. Two planets are connected by a special undirected path. Both the players can travel through these paths only. Configuration represents a tree.
+
+A tree is a connected undirected graph with no cycles.
+
+In each move, a player is allowed to move to any of the empty adjacent planet from his/her current position. The player cannot be stationary during his/her move. Player loses if he/she has no more moves to make . Find the winner of the game if both players play optimally.
+
+Cipher1 plays first.
+
+A planet is empty if there is no player in it.
+
+Input Format
+
+The first line of the input contains the number of test cases t.
+
+For each testcase, first line contains the number of planets n.
+
+The next n-1 lines contain the pair of the planets (a,b) which are directly connected by a special path.
+
+The last line contains the starting position of the Cipher1 and Cipher2, x and y respectively.
+
+Constraints
+
+1 <= t <= 2*104
+
+1 <= n <= 105
+
+1 <= a, b, x , y <= n
+
+a != b
+
+Sum of n over all test cases does not exceed 2*105
+
+Output Format
+
+For each test case print the winner of the game.
+
+Output is Case Sensitive
+
+Sample Input 0
+3
+4
+2 1
+4 3
+3 1
+1 4
+2
+1 2
+1 2
+4
+3 1
+2 4
+3 4
+2 4
+Sample Output 0
+
+Cipher1
+Cipher2
+Cipher2
+Explanation 0
+
+In the first testcase if Cipher1 moves from planet 1 to planet 3 then Cipher2 would have no more moves left.
+
+In the second testcase Cipher1 has no moves since both the planets are occupied hence Cipher2 wins.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 // OP OR WOT
